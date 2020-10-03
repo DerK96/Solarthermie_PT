68c6292fb0cb965aeb845e11ba69693682337550
Philipp Tegtmeyer <philipp.tegtmeyer@alumni.fh-aachen.de>
2020-10-03 13:36:00 +0200

    added MLAB Files
    mvd content

---
+++ Druckverlust_und_hydraulischer_Abgleich/MLAB/druckverlust.m
@@ -0,0 +1,56 @@
+%Druckverlust
+%% Init
+clc;
+clear;
+close all;
+%% Import Data
+d1.h600 = importfile('../DATA/Volumenstrom600l-h_07.09.2020 09_36_04.csv');
+d1.h1310 = importfile('../DATA/Volumenstrom1310l-h_07.09.2020 09_31_25.csv');
+d1.h2147 = importfile('../DATA/Volumenstrom2147l-h_07.09.2020 09_25_42.csv');
+d1.h2855 = importfile('../DATA/Volumenstrom2855l-h_07.09.2020 09_14_37.csv');
+%% init constants
+NennVp = [600;1310;2147;2855];  %[L/h]
+NennVpSI = NennVp./3600000;     %[m^3/s]
+%% calc dP-Loss
+fn = fieldnames(d1);
+for i=1:numel(fn)
+    meanDP(:,i) = mean(table2array(d1.(fn{i})(:,15)));
+end
+
+meanDP = meanDP.*100; %[mbar] -> [pa]
+dPBhs = 0.000155*(NennVp.^2)+0.0967*NennVp-37.7; %[pa]
+dPBhs = dPBhs';
+
+dPKr = meanDP - dPBhs;
+
+%% plot Data
+figure
+hold on
+plot(NennVp,dPKr,'-x');
+grid on
+% xlim([0 660])
+xlabel('Durchfluss [$\frac{L}{h}$]')
+ylabel('Druckverlust [$pa$]')
+% legend('Vortex','US stat','MID','US mobil','RKZ','location','best')
+run plotsettings.m
+printPath = '../DATA/dPPlot';
+print(printPath,'-depsc');
+
+%% Rohrreibung
+meanDP = meanDP';
+d1 = 19.3*10^-3;
+d2 = 33*10^-3;
+A1 = ((d1/2)^2)*pi;
+A2 = ((d2/2)^2)*pi;
+t = 25;
+l = 1.1;
+rho = 1000.6-0.0426*t-0.0041*t^2;
+v_inf = NennVpSI ./ A1; %[m/s]
+q = (rho/2).*(v_inf.^2);
+eta = (554.78+19.73*t+0.128*t^2-2.69*10^-4+t^3)^-1;
+Re = (v_inf.*d1*rho)/eta;
+lambda = (meanDP./q)*(d1/l);
+dA = A1/A2;
+zetaAng = 0.08;
+zeta = 2*zetaAng;
+
---
+++ Durchflussmessung/MLAB/durchflussmessung.m
@@ -0,0 +1,88 @@
+% Durchflussmessung
+%% Init
+clc;
+clear;
+close all;
+%% Import Data
+d.h060 = importfile('../DATA/060l-h_07.09.2020 16_15_40.csv');
+d.h120 = importfile('../DATA/120l-h_07.09.2020 16_27_41.csv');
+d.h240 = importfile('../DATA/240l-h_07.09.2020 16_35_40.csv');
+d.h360 = importfile('../DATA/360l-h_07.09.2020 16_42_15.csv');
+d.h480 = importfile('../DATA/480l-h_07.09.2020 16_47_24.csv');
+d.h660 = importfile('../DATA/660l-h_07.09.2020 16_53_33.csv');
+tData = importfile('../DATA/Versuch2_Temperatur_07.09.2020 16_59_22.csv');
+%% Process Data
+Sensor = [3 5 7 9 11 13 15];
+sNames = fieldnames(d);
+for i = 1:numel(sNames)
+    s = size(d.(sNames{i}));
+    FT = d.(sNames{i}).Zeit(s) - d.(sNames{i}).Zeit(1);
+    for k = 1:size(Sensor,2)
+%         if k == 1
+%             for j = 1:size(d.(sNames{i})(:,Sensor(k)),1)
+%                 if table2array(d.(sNames{i})(j,Sensor(k))) < 54
+%                     d.(sNames{i})(j,Sensor(k)) = array2table(54);
+%                 end
+%             end
+%         end
+        if k == 6 
+            for j = 1:size(d.(sNames{i})(:,Sensor(k)),1)
+                d.(sNames{i})(j,Sensor(k)) = array2table((table2array(d.(sNames{i})(j,Sensor(k)))*1000));
+            end
+        end
+        if k == 7 
+            for j = 1:size(d.(sNames{i})(:,Sensor(k)),1)
+                d.(sNames{i})(j,Sensor(k)) = array2table((table2array(d.(sNames{i})(j,Sensor(k)))/table2array(d.(sNames{i})(j,1))*3600));
+            end
+        end
+        meanData(i,k) = mean(table2array(d.(sNames{i})(:,Sensor(k))));
+        sigData(i,k) = std(table2array(d.(sNames{i})(:,Sensor(k))));
+    end
+end
+%% calc refvalues
+wiegeverfahren;
+%% calc percentual deviation 
+for n = 1 : size(meanData,2)%Sensoren
+    for m = 1 : size(meanData,1)%Datensaetzen
+        deviPc(m,n) = ((dfLpH(:,m)-meanData(m,n))/dfLpH(:,m))*100;
+    end
+end
+%% plot deviation
+figure
+hold on
+plot(dfLpH,deviPc(:,1),'-x');
+plot(dfLpH,deviPc(:,2),'-.o');
+plot(dfLpH,deviPc(:,5),':+');
+plot(dfLpH,deviPc(:,6),'--s');
+plot(dfLpH,deviPc(:,7),'-^');
+grid on
+xlim([0 660])
+xlabel('wahrer Durchfluss [$\frac{L}{h}$]')
+ylabel('prozentuale Abweichung [\%]')
+legend('Vortex','US stat','MID','US mobil','RKZ','location','best')
+run plotsettings.m
+printPath = '../DATA/devPcPlot';
+print(printPath,'-depsc');
+%% plot Temperatures
+figure
+hold on
+plot(tData.Scan,tData.T1C,'-x');
+plot(tData.Scan,tData.T2C,'-.o');
+grid on
+%xlim([0 660])
+xlabel('elapsed Time [s]')
+ylabel('Temperature [$^{\circ} C$]')
+legend('T1','T2','location','best')
+run plotsettings.m
+printPath = '../DATA/tempPlot';
+print(printPath,'-depsc');
+%% calc V dot with tempdev
+
+tempDev = 90;       %[s]
+dRohr = 20*10^-3;   %[m]
+lRohr = 9.48;       %[m]
+
+vPs = ((pi*(dRohr^2))*lRohr)/tempDev;
+vPh = vPs*3600000;
+%% Vdot for comparison (source MID)
+VpMID = mean(tData.MIDinlproh);
