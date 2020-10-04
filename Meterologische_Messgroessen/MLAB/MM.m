81abad459434880ae2dcbef5b81e46959662ec91
LosGroschos <chiller108@googlemail.com>
2020-10-04 08:44:29 +0200

    Auswertung bis Teil E fertig

--- Meterologische_Messgroessen/MLAB/MM.m
+++ Meterologische_Messgroessen/MLAB/MM.m
@@ -4,9 +4,12 @@ clear;
 close all;
 %% Import Data
 d.Radiation = importfile2('../200909_MeteoMess_Teil A+B_Solarstrahlung.xlsx'); %aus überordner
+d.Response = importfile3('../190919_MeteoMess_Teil B_Ansprechzeiten.xlsx')
 d.Humidity = importfile('../Niederschlag_Luftfeuchte_ 04.09.2020 14_34_57.csv');
-%% Darstellung Messreihe
+d.Humidity2 = importfile('../Relative Luftfeucht_Teil_C04.09.2020 14_18_43.csv');
+%% Versuchsteil A
 names = {'TA GBS','SSR81','CM3','CM11 gesamt','CM11 diffus'};
+
 t1 = d.Radiation.Scan;
 TAGBS = d.Radiation.TAGBS;
 SSR81 = d.Radiation.ENNOSSSR81;
@@ -16,72 +19,109 @@ CM11dif = d.Radiation.CM11Schattenring;
 
 figure
 hold on
+grid on
 plot(t1,TAGBS);
 plot(t1,SSR81);
 plot(t1,CM3);
 plot(t1,CM11ges);
 plot(t1,CM11dif);
 xlabel('Scan [s]')
-ylabel('Strahlung [$\frac{W}{m^2}$]')
+ylabel('Strahlung $\left[\frac{W}{m^2}\right]$')
 legend(names,'location','best')
 run plotsettings.m
-%print('Messreihe_Strahlung','-depsc');
+print('../DATA/Messreihe_Strahlung.eps','-depsc');
 hold off
 
-%% Versuchsteil A (und B)
 %Scan 170-220
 
-dataPoints = 1:2:19; %start:step:stop
+dataPoints = 3:2:19; %start:step:end
 fn = fieldnames(d); % Spaltentitel abrufen
 
-for i = 1:numel(fn) %1 bis 4 numel=Anzahl Elemente von fn
-    %d.(fn{i})(:,3) = array2table(table2array(d.(fn{i})(:,3)) - offset);
-    for k = 1:numel(dataPoints)
-        curr = table2array(d.(fn{i})(:,11:19));
-        m2(k,i) = mean(curr(size(curr,1)-20:size((curr),1),dataPoints(:,k)));
-       % m3(k,i) = std(curr(170:220),dataPoints(:,k)));
-    end
+for k = 1:numel(dataPoints) %Zeige Mittelwerte in m2, Standardabweichung in s2
+     curr = table2array(d.Radiation(:,dataPoints));
+     m2(:,k) = mean(curr(170:220,k));
+     s2(:,k) = std(curr(170:220,k));
 end
 
-disp(m2);
-%disp(m3);
 
-%Edir = m2(x,y)-m2(x,y)
+disp(m2);
+disp(s2);
+
+Edir = m2(1,8)-m2(1,9) %Berechnung direktstrahlung aus Mittelwerten CM11
+
+%% Versuchsteil B
+
+names2 = {'TA GBS','SSR81','CM3','CM11'};
+
+t2 = d.Response.Scan;
+TAGBSres = d.Response.TAGBS;
+SSR81res = d.Response.ENNOSSSR81;
+CM3res = d.Response.CM3;
+CM11res = d.Response.CM11;
+
+
+figure
+hold on
+grid on
+plot(t2,TAGBSres);
+plot(t2,SSR81res);
+plot(t2,CM3res);
+plot(t2,CM11res);
+xlabel('Scan [s]')
+ylabel('Strahlung $\left[\frac{W}{m^2}\right]$')
+legend(names2,'location','best')
+run plotsettings.m
+print('../DATA/Messreihe_Ansprechzeit.eps','-depsc');
+hold off
 
 %% Versuchsteile C und D
 
-AvTemp = mean(d.Radiation.TemperatureC);
-AvHum = mean(d.Radiation.RelativeFeuchteinV);
-StdTemp = std(d.Radiation.TemperatureC);
-StdHum = std(d.Radiation.RelativeFeuchteinV);
+AvTemp = mean(d.Humidity2.TemperatureC);
+StdAvTemp = std(d.Humidity2.TemperatureC);
+AvHum = mean(d.Humidity2.RelativeFeuchteinV);
+StdAvHum = std(d.Humidity2.RelativeFeuchteinV);
+StdTemp = std(d.Humidity2.TemperatureC);
+StdHum = std(d.Humidity2.RelativeFeuchteinV);
 
 t2 = d.Humidity.Scan;
 Hum = d.Humidity.RelativeFeuchteinV;
 HumNaCl = mean(Hum(1400:1500));
 HumLiCl = mean(Hum(3024:3124));
+StdNaCl = std(Hum(1400:1500));
+StdLiCl = std(Hum(3024:3124));
 
 
-
-figure % Zeige Messverlauf der Feuchtemessung
+names3 = {'Temperatur','Spannung'}
+figure % Messverlauf der Temperatur/Feuchtemessung
 hold on
-plot(t2,Hum);
+plot(d.Humidity2.Scan,d.Humidity2.TemperatureC)
+plot(d.Humidity2.Scan,d.Humidity2.RelativeFeuchteinV)
+xlabel('Scan [s]')
+ylabel('Rel. Feuchte [V] / Temperatur [$^{\circ}C$]')
+legend(names3,'location','best')
+run plotsettings.m
+print('../DATA/Messreihe_Umgebung.eps','-depsc');
+hold off
+
+FitHum = [HumLiCl,12;HumNaCl,75]; %Gemessene Spannungen und Literaturwerte
+
+figure
+hold on
+plot(t2,Hum)
 xlabel('Scan [s]')
 ylabel('Spannung [V]')
 run plotsettings.m
-%print('Messreihe_Feuchte','-depsc');
+print('../DATA/Messreihe_Feuchtekalibration.eps','-depsc');
 hold off
 
-FitHum = [0.11,HumLiCl;0.75,HumNaCl]; %Gemessene Spannungen und Literaturwerte
-
-
-figure %
+figure 
 f = fit(FitHum(:,1),FitHum(:,2),'poly1')
 hold on
-plot(f)
-plot(FitHum(:,1),FitHum(:,2))
-xlabel('Relative Luftfeuchte [%]')
-ylabel('Spannung [V]')
+plot(f,FitHum(:,1),FitHum(:,2))
+legend('location','best')
+ylabel('Relative Luftfeuchte [\%]')
+xlabel('Spannung [V]')
 run plotsettings.m
-%print('Kalibriergerade_Feuchte','-depsc');
+print('../DATA/Kalibriergerade_Feuchte.eps','-depsc');
 
-
+%% Versuchsteil E
