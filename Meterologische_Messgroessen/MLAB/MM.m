c35cef97fc70f3519d36e66acc691f4c9e53c960
Philipp Tegtmeyer <philipp.tegtmeyer@alumni.fh-aachen.de>
2020-10-25 15:33:40 +0100

    meteo finalized

--- Meterologische_Messgroessen/MLAB/MM.m
+++ Meterologische_Messgroessen/MLAB/MM.m
@@ -4,7 +4,7 @@ clear;
 close all;
 %% Import Data
 d.Radiation = importfile2('../200909_MeteoMess_Teil A+B_Solarstrahlung.xlsx'); %aus überordner
-d.Response = importfile3('../190919_MeteoMess_Teil B_Ansprechzeiten.xlsx')
+d.Response = importfile3('../190919_MeteoMess_Teil B_Ansprechzeiten.xlsx');
 d.Humidity = importfile('../Niederschlag_Luftfeuchte_ 04.09.2020 14_34_57.csv');
 d.Humidity2 = importfile('../Relative Luftfeucht_Teil_C04.09.2020 14_18_43.csv');
 d.Wind = importfile4('../200901_Winddaten.xlsx');
@@ -17,7 +17,7 @@ SSR81 = d.Radiation.ENNOSSSR81;
 CM3 = d.Radiation.CM3;
 CM11ges = d.Radiation.CM11;
 CM11dif = d.Radiation.CM11Schattenring;
-
+%% start first plot
 figure
 hold on
 grid on
@@ -26,10 +26,10 @@ plot(t1,SSR81);
 plot(t1,CM3);
 plot(t1,CM11ges);
 plot(t1,CM11dif);
-xlabel('elapsed time [t] in s')
-ylabel('Strahlung in $\frac{W}{m^2}$')
+xlabel('vergangene Zeit [t] = s')
+ylabel('Bestrahlung [E] = $\frac{W}{m^2}$')
 pbaspect([3 1 1])
-legend(names,'location','best')
+legend(names,'location','eastoutside')
 run plotsettings.m
 print('../DATA/Messreihe_Strahlung.eps','-depsc');
 hold off
@@ -49,7 +49,7 @@ end
 disp(m2);
 disp(s2);
 
-Edir = m2(1,8)-m2(1,9) %Berechnung direktstrahlung aus Mittelwerten CM11
+Edir = m2(1,8)-m2(1,9); %Berechnung direktstrahlung aus Mittelwerten CM11
 
 %% Versuchsteil B
 
@@ -61,7 +61,6 @@ SSR81res = d.Response.ENNOSSSR81;
 CM3res = d.Response.CM3;
 CM11res = d.Response.CM11;
 
-
 figure
 hold on
 grid on
@@ -69,10 +68,10 @@ plot(t2,TAGBSres);
 plot(t2,SSR81res);
 plot(t2,CM3res);
 plot(t2,CM11res);
-xlabel('elapsed time [t] in s')
-ylabel('Strahlung $\frac{W}{m^2}$')
+xlabel('vergangene Zeit [t] = s')
+ylabel('Bestrahlung [E] = $\frac{W}{m^2}$')
 pbaspect([3 1 1])
-legend(names2,'location','east')
+legend(names2,'location','eastoutside')
 run plotsettings.m
 print('../DATA/Messreihe_Ansprechzeit.eps','-depsc');
 hold off
@@ -94,14 +93,14 @@ StdNaCl = std(Hum(1400:1500));
 StdLiCl = std(Hum(3024:3124));
 
 
-names3 = {'Temperatur','Spannung'}
+names3 = {'Temperatur','Spannung'};
 figure % Messverlauf der Temperatur/Feuchtemessung
 hold on
 grid on
 plot(d.Humidity2.Scan,d.Humidity2.TemperatureC)
 plot(d.Humidity2.Scan,d.Humidity2.RelativeFeuchteinV)
-xlabel('elapsed time [t] in s')
-ylabel('Rel. Feuchte [U] in V / Temp. [T] in $^{\circ}C$')
+xlabel('vergangene Zeit [t] = s')
+ylabel('Rel. Feuchte [U] = V / Temp. [T] = $^{\circ}C$')
 pbaspect([3 1 1])
 legend(names3,'location','east')
 run plotsettings.m
@@ -114,21 +113,21 @@ figure
 hold on
 grid on
 plot(t3,Hum)
-xlabel('elapsed time [t] in s')
-ylabel('Spannung [U] in V')
+xlabel('vergangene Zeit [t] = s')
+ylabel('Spannung [U] = V')
 pbaspect([3 1 1])
 run plotsettings.m
 print('../DATA/Messreihe_Feuchtekalibration.eps','-depsc');
 hold off
 
 figure 
-f = fit(FitHum(:,1),FitHum(:,2),'poly1')
+f = fit(FitHum(:,1),FitHum(:,2),'poly1');
 hold on
 grid on
 plot(f,FitHum(:,1),FitHum(:,2))
 legend('location','southeast')
 ylabel('Relative Luftfeuchte in \%')
-xlabel('Spannung [U] in V')
+xlabel('Spannung [U] = V')
 pbaspect([3 1 1])
 run plotsettings.m
 print('../DATA/Kalibriergerade_Feuchte.eps','-depsc');
@@ -144,12 +143,12 @@ grid on
 plot(t3,Rain)
 %legend('location','best')
 ylabel('Anzahl Impulse')
-xlabel('elapsed time [t] in s')
+xlabel('vergangene Zeit [t] = s')
 pbaspect([3 1 1])
 run plotsettings.m
 print('../DATA/Messreihe_Niederschlag.eps','-depsc');
 
-SensArea = 3.1415*(0.165/2)^2
+SensArea = 3.1415*(0.165/2)^2;
 
 %% Versuchsteil F
 
@@ -161,8 +160,8 @@ figure
 grid on
 plot(tWind,Windspd)
 %legend('location','best')
-ylabel('Windgeschwindigkeit [v] in $\frac{m}{s}$')
-xlabel('elapsed time [t] in s')
+ylabel('Windgeschwindigkeit [v] = $\frac{m}{s}$')
+xlabel('vergangene Zeit [t] = s')
 run plotsettings.m
 print('../DATA/Windgeschwindigkeit.eps','-depsc');
 
@@ -170,19 +169,19 @@ figure
 grid on
 plot(tWind,Winddir)
 %legend('location','best')
-ylabel('Windrichtung in $^{\circ}$')
-xlabel('elapsed time [t] in s')
+ylabel('Windrichtung [DEG] = $^{\circ}$')
+xlabel('vergangene Zeit [t] = s')
 run plotsettings.m
 print('../DATA/Windrichtung.eps','-depsc');
 
 %Windrichtung Verteilung
 figure 
 grid on
-edges = [0 46 91 136 181 226 271 316 359]
+edges = [0 46 91 136 181 226 271 316 359];
 histogram(Winddir,8,'Normalization','probability','BinEdges',edges)
 %legend('location','best')
-xlabel('Windrichtung in $^{\circ}$')
-ylabel('Relative Häufigkeit')
+xlabel('Windrichtung [DEG] = $^{\circ}$')
+ylabel('Relative Haeufigkeit')
 run plotsettings.m
 print('../DATA/WinddirCN.eps','-depsc');
 
@@ -193,6 +192,6 @@ grid on
 histogram(Windspd,8,'BinWidth',1,'Normalization','probability')
 %legend('location','best')
 xlabel('Windrichtung in Klassen')
-ylabel('Relative Häufigkeit')
+ylabel('Relative Haeufigkeit')
 run plotsettings.m
 print('../DATA/WindspdCN.eps','-depsc');
